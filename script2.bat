5f6e833d06d867704aea0eaa264612ce74685400
lizethpuc1 <lizethalexandrapuc@gmail.com>
2025-11-10 18:29:09 -0600

    ejemplo2

---
+++ script2.bat
@@ -0,0 +1,6 @@
+@echo off
+echo ======================================
+echo Hola desde el segundo ejemplo!
+echo ======================================
+mkdir resultados
+echo Este archivo fue generado por Jenkins (Ejemplo 2) > resultados\salida2.txt
