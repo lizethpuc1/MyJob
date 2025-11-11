157a37ac09afadb85582eb01fe3bac8219e9ac17
lizethpuc1 <lizethalexandrapuc@gmail.com>
2025-11-10 18:34:28 -0600

    Fix mkdir command in script2.bat

--- script2.bat
+++ script2.bat
@@ -2,5 +2,6 @@
 echo ======================================
 echo Hola desde el segundo ejemplo!
 echo ======================================
-mkdir resultados
+if not exist resultados mkdir resultados
 echo Este archivo fue generado por Jenkins (Ejemplo 2) > resultados\salida2.txt
+
